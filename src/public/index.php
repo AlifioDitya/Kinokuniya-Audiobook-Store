79c3b0cd364cede397dd3cc2c7709309a839ec9c
Alifioditya <alifioditya@gmail.com>
2023-10-01 19:03:13 +0700

    feat: add card grid section to dashboard

--- src/public/index.php
+++ src/public/index.php
@@ -117,7 +117,7 @@
                     </div>
                 </section>
                 <section class="dashboard-section">
-                    <div class="tertiary-container">
+                    <div>
                         <div class="card-header">
                             <h4 class="dashboard-header">Newest Releases</h4>
                             <a href="google.com">
@@ -182,22 +182,66 @@
                             </div>
                         </div>
                     </div>
+                    <div>
+                        <div class="card-header">
+                            <h4 class="dashboard-header">Owned Books</h4>
+                            <a href="google.com">
+                                <p class="see-all">See All</p>
+                            </a>
+                        </div>
+                        <div class="card-grid-pagination">
+                            <div class="book-card-brief">
+                                <a>
+                                    <img class="book-img-brief" src="images/klara.svg" alt="Book Image">
+                                </a>
+                                <div class="book-card-brief-desc">
+                                    <h4 class="book-card-title">Klara and the Sun</h4>
+                                    <p class="book-card-author">by Kazuo Ishiguro</p>
+                                    <p class="book-card-price">Rp 150.000</p>
+                                </div>
+                            </div>
+                            <div class="book-card-brief">
+                                <a>
+                                    <img class="book-img-brief" src="images/cantik-itu-luka.svg" alt="Book Image">
+                                </a>
+                                <div class="book-card-brief-desc">
+                                    <h4 class="book-card-title">Cantik itu Luka</h4>
+                                    <p class="book-card-author">by Eka Kurniawan</p>
+                                    <p class="book-card-price">Rp 150.000</p>
+                                </div>
+                            </div>
+                            <div class="book-card-brief">
+                                <a>
+                                    <img class="book-img-brief" src="images/laut-bercerita.svg" alt="Book Image">
+                                </a>
+                                <div class="book-card-brief-desc">
+                                    <h4 class="book-card-title">Laut Bercerita</h4>
+                                    <p class="book-card-author">by Leila S. Chudori</p>
+                                    <p class="book-card-price">Rp 150.000</p>
+                                </div>
+                            </div>
+                            <div class="book-card-brief">
+                                <a>
+                                    <img class="book-img-brief" src="images/nebula.svg" alt="Book Image">
+                                </a>
+                                <div class="book-card-brief-desc">
+                                    <h4 class="book-card-title">Nebula</h4>
+                                    <p class="book-card-author">by Tere Liye</p>
+                                    <p class="book-card-price">Rp 150.000</p>
+                                </div>
+                            </div>
+                            <div class="book-card-brief">
+                                <a>
+                                    <img class="book-img-brief" src="images/rich-people-problem.svg" alt="Book Image">
+                                </a>
+                                <div class="book-card-brief-desc">
+                                    <h4 class="book-card-title">Rich People Problem</h4>
+                                    <p class="book-card-author">by Kevin Kwan</p>
+                                    <p class="book-card-price">Rp 150.000</p>
+                                </div>
+                            </div>
+                        </div>
                 </section>
-                <div class="testing">
-
-                </div>
-                <div class="testing">
-
-                </div>
-                <div class="testing">
-
-                </div>
-                <div class="testing">
-
-                </div>
-                <div class="testing">
-
-                </div>
             </div>
         </main>
     </div>
