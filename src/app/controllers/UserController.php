be6c6cdecd1ffb981384ec039cd8e87f1ae350b8
Alifioditya <alifioditya@gmail.com>
2023-10-02 21:09:38 +0700

    feat: add middleware and setup user controller for register and login purposes

---
+++ src/app/controllers/UserController.php
@@ -0,0 +1,175 @@
+<?php
+
+class UserController extends Controller implements ControllerInterface
+{
+    public function index()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    // Prevent Access except Admin
+                    $authMiddleware = $this->middleware('AuthenticationMiddleware');
+                    $authMiddleware->isAdmin();
+
+                    // Grab user data
+                    $userModel = $this->model('UserModel');
+                    $res = $userModel->getUsers(1);
+
+                    // Keperluan navbar
+                    if (isset($_SESSION['user_id'])) {
+                        // Ada data user_id, coba fetch data username!
+                        $userModel = $this->model('UserModel');
+                        $user = $userModel->getUserFromID($_SESSION['user_id']);
+                        $nav = ['username' => $user->username, 'is_admin' => $user->is_admin];
+                    } else {
+                        $nav = ['username' => null];
+                    }
+
+                    $userListView = $this->view('user', 'UserListView', array_merge($nav, $res));
+                    $userListView->render();
+                    exit;
+
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            if ($e->getCode() == 401) {
+                /* Unauthorized */
+                $notFoundView = $this->view('not-found', 'NotFoundView');
+                $notFoundView->render();
+            }
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+
+    public function fetch($page)
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    $userModel = $this->model('UserModel');
+                    $res = $userModel->getUsers((int) $page);
+
+                    header('Content-Type: application/json');
+                    http_response_code(200);
+                    echo json_encode($res);
+                    exit;
+
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+
+    public function login()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+
+                    $loginView = $this->view('user', 'LoginView');
+                    $loginView->render();
+                    exit;
+
+                case 'POST':
+
+                    $userModel = $this->model('UserModel');
+                    $userId = $userModel->login($_POST['username'], $_POST['password']);
+                    $_SESSION['user_id'] = $userId;
+
+                    // Return redirect_url
+                    header('Content-Type: application/json');
+                    http_response_code(201);
+                    echo json_encode(["redirect_url" => BASE_URL . "/home"]);
+                    exit;
+
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+
+    public function logout()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'POST':
+
+                    unset($_SESSION['user_id']);
+
+                    // Return redirect_url
+                    header('Content-Type: application/json');
+                    http_response_code(201);
+                    echo json_encode(["redirect_url" => BASE_URL . "/user/login"]);
+                    exit;
+
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+
+    public function register()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+
+                    $registerView = $this->view('user', 'RegisterView');
+                    $registerView->render();
+                    exit;
+
+                case 'POST':
+
+                    $userModel = $this->model('UserModel');
+                    $userModel->register($_POST['email'], $_POST['username'], $_POST['password'], $_POST['fullname']);
+
+                    // Return redirect_url
+                    header('Content-Type: application/json');
+                    http_response_code(201);
+                    echo json_encode(["redirect_url" => BASE_URL . "/user/login"]);
+                    exit;
+
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+        }
+    }
+
+    public function username()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+
+                    $userModel = $this->model('UserModel');
+                    $user = $userModel->doesUsernameExist($_GET['username']);
+
+                    if (!$user) {
+                        throw new LoggedException('Not Found', 404);
+                    }
+
+                    http_response_code(200);
+                    exit;
+
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+}
--- src/app/core/Controller.php
+++ src/app/core/Controller.php
@@ -8,9 +8,15 @@ class Controller
         return new $view($data);
     }
 
-    // public function model($model)
-    // {
-    //     require_once __DIR__ . '/../models/' . $model . '.php';
-    //     return new $model();
-    // }
+    public function model($model)
+    {
+        require_once __DIR__ . '/../models/' . $model . '.php';
+        return new $model();
+    }
+
+    public function middleware($middleware)
+    {
+        require_once __DIR__ . '/../middlewares/' . $middleware . '.php';
+        return new $middleware();
+    }
 }
