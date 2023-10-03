8b4a93b70268f0e0a84b2b09fbd4e064fb98ae2d
ezra hutagaol <13521073@std.stei.itb.ac.id>
2023-10-03 16:43:32 +0700

    add user control page by admin

---
+++ src/app/controllers/UserListController.php
@@ -0,0 +1,23 @@
+<?php
+
+class UserListController extends Controller implements ControllerInterface
+{
+    public function index()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+
+                    $homeView = $this->view('user', 'UserListView');
+                    $homeView->render();
+
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+        }
+    }
+    
+}
